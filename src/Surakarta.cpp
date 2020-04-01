e5bf22b5d728eeb44a73431eafe58ebfa6ef0873
Shukant Pal <shukantpal@outlook.com>
2020-04-01 14:43:11 -0400

    Skeleton

---
+++ src/Surakarta.cpp
@@ -0,0 +1,28 @@
+/**
+ * Surakarta.cpp
+ *
+ * Copyright (C) 2020 Shukant Pal <shukantpal@outlook.com>
+ *
+ * This file is part of libsurakarta.
+ *
+ * libsurakarta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libsurakarta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libsurakarta.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @author Shukant Pal <shukantpal@outlook.com>
+ */
+
+#include <Surakarta.hpp>
+
+Surakarta::Surakarta(bool init)
+{
+}
